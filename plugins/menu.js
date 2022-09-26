ae2090cc8fe36b4bdce4260db118e06496d84660
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-09-26 10:21:56 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -161,7 +161,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
 handler.help = ['menu', 'help', '?']
 handler.tags = ['main']
 handler.command = /^(menu|help|\?)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
