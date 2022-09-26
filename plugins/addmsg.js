dc90ed19ba8cdd476c69cb2723276b9142115558
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-09-26 10:03:37 +0700

    Update addmsg.js

--- plugins/addmsg.js
+++ plugins/addmsg.js
@@ -4,9 +4,9 @@ let handler = async (m, { conn, command, usedPrefix, text }) => {
     let M = WAMessageProto.WebMessageInfo
     let which = command.replace(/\+|add/i, '')
     if (!m.quoted) throw 'Balas pesan!'
-    if (!text) return conn.sendButton(m.chat, `Uhm.. teksnya mana?\n\nContoh:\n${usedPrefix + command} tes`, '© wabot-aq', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
+    if (!text) return conn.sendButton(m.chat, `Uhm.. teksnya mana?\n\nContoh:\n${usedPrefix + command} tes`, '© Syn', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
     let msgs = db.data.msgs
-    if (text in msgs) return conn.sendButton(m.chat, `'${text}' telah terdaftar, gunakan nama lain!`, '© wabot-aq', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
+    if (text in msgs) return conn.sendButton(m.chat, `'${text}' telah terdaftar, gunakan nama lain!`, '© Syn', 'Daftar Pesan', `${usedPrefix}list${which}`, m)
     msgs[text] = M.fromObject(await m.getQuotedObj()).toJSON()
     if (db.data.chats[m.chat].getmsg) return m.reply(`Berhasil menambahkan pesan dengan nama '${text}'
     
@@ -15,7 +15,7 @@ Akses dengan mengetik '${text}'`)
 
 Akses dengan ${usedPrefix}get${which} ${text}
 
-Jika Getmsg diaktifkan maka tidak perlu lagi mengetik *${usedPrefix}get${which}*`, '© wabot-aq', 'Aktifkan', '.1 getmsg', m)
+Jika Getmsg diaktifkan maka tidak perlu lagi mengetik *${usedPrefix}get${which}*`, '© Syn', 'Aktifkan', '.1 getmsg', m)
 }
 handler.help = ['vn', 'msg', 'video', 'gif', 'audio', 'img', 'sticker'].map(v => 'add' + v + ' <teks>')
 handler.tags = ['database']
